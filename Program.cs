af291455bb98e6095f662f2b29e574ccb3a22460
mustafaberk01 <mustafaberkay5234@gmail.com>
2023-01-21 16:05:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class ArrayRotator
+{
+    static void RotateArrayLeft(int[] input)
+    {
+        for (int i = 0; i < input.Length - 1; i++)
+        {
+            int temp = input[i];
+            input[i] = input[i + 1];
+            input[i + 1] = temp;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1,2,3,4,5,6 };
+        RotateArrayLeft(arr);
+        Array.ForEach(arr, Console.WriteLine);
+    }
+}
